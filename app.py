78aad3bc36cb18167bfc72073d1f5b94e05fa300
lukaszkuczynski <kuczynskilukasz@gmail.com>
2018-01-04 17:02:28 +0100

    Validation done against rules provided as env var

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import yaml
 from validator import validate
 from rule import ValidationException
+import os
 
 app = Flask(__name__)
 
@@ -15,13 +16,24 @@ def hello_world():
 @app.route('/validate', methods=['POST'])
 def validate_endpoint():
     documents = request.get_json(force=True)
-    rules = read_yaml_rules(RULES_FILE_NAME)
+    rules = get_rules_dict()
     try:
         validate(docs=documents, rule_definitions=rules)
         return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
     except ValidationException as e:
         return json.dumps({'success': False, 'msg': e.__str__()}), 500, {'ContentType': 'application/json'}
 
+@app.route("/rules")
+def get_rules():
+    rules = get_rules_dict()
+    return json.dumps(rules)
+
+
+def get_rules_dict():
+    rules_json = os.getenv("RULES_JSON", "[]")
+    print(rules_json)
+    rules = json.loads(rules_json)
+    return rules
 
 
 def read_yaml_rules(filename):
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export FLASK_DEBUG=1
 export FLASK_APP=app.py
+export RULES_JSON="{\"first_name_not_null\":{\"type\":\"not_null\",\"fields\":[\"first_name\"]},\"age_cant_be_missing\":{\"type\":\"missing\",\"fields\":[\"age\"]}}"
 flask run
